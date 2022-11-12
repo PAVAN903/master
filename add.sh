1cc427e36d6c1aa768d4e939d645f534b05b4805
PAVAN903 <navap903@gmail.com>
2022-11-12 07:19:51 +0530

    morningreview

---
+++ add.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+     toss=$((RANDOM%2))
+
+         if(($toss ==1))
+              then
+                echo "heads";
+              else
+                echo "tail"
+             fi
